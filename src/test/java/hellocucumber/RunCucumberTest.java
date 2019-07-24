ff2e143f2cd4d649209dfca8283471e2e8f7a952
AlexandrNedilko <aleksandr_ned@ukr.net>
2019-07-24 22:37:15 +0300

    Initial commit

---
+++ src/test/java/hellocucumber/RunCucumberTest.java
@@ -0,0 +1,12 @@
+package hellocucumber;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+
+@CucumberOptions(features = "src/main/resources/features",
+        monochrome = true,
+        tags = "@TestScenarioV1",
+        glue={"steps"})
+public class RunCucumberTest extends AbstractTestNGCucumberTests{
+}
