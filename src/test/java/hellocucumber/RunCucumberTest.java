e6f0dff3f7816869f405c1ffc6003bf28f17a30a
AlexandrNedilko <aleksandr_ned@ukr.net>
2019-08-01 20:40:21 +0300

    cucumber new

--- src/test/java/hellocucumber/RunCucumberTest.java
+++ src/test/java/hellocucumber/RunCucumberTest.java
@@ -4,7 +4,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 
-@CucumberOptions(features = "src/test/resources/hellocucumber/features",
+//@CucumberOptions(features = "src/test/resources/hellocucumber/features",
+        @CucumberOptions(features = "src/test/resources/hellocucumber/myfirststeps.features",
         monochrome = true,
         tags = "@TestScenarioV1",
         glue={"steps"})
